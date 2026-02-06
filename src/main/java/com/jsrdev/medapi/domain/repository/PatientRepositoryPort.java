870656b08dd2ee062cc8e90a03190df777bb7ce0
josesreyes <josesreyes.jsr@gmail.com>
2026-02-06 17:52:54 -0600

    Add get active patients and active by id

--- src/main/java/com/jsrdev/medapi/domain/repository/PatientRepositoryPort.java
+++ src/main/java/com/jsrdev/medapi/domain/repository/PatientRepositoryPort.java
@@ -3,6 +3,11 @@ package com.jsrdev.medapi.domain.repository;
 import com.jsrdev.medapi.domain.common.Email;
 import com.jsrdev.medapi.domain.common.PhoneNumber;
 import com.jsrdev.medapi.domain.model.patient.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public interface PatientRepositoryPort {
     Patient create(Patient patient);
@@ -12,4 +17,8 @@ public interface PatientRepositoryPort {
     boolean existsByIdentityDocument(String identityDocument);
 
     boolean existsByPhoneNumber(PhoneNumber phoneNumber);
+
+    Page<Patient> findActivePatients(Pageable pageable);
+
+    Optional<Patient> findById(UUID id);
 }
